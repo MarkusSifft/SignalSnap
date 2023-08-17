8fd67275049b41dacb9f1a8ec1a49c2318b03fbb
Markus Sifft <m.sifft@icloud.com>
2023-08-17 12:09:55 +0200

    Updated Licence and TOML

--- src/signalsnap/spectrum_calculator.py
+++ src/signalsnap/spectrum_calculator.py
@@ -1,35 +1,9 @@
 # This file is part of signalsnap: Signal Analysis In Python Made Easy
+# Copyright (c) 2020 and later, Markus Sifft and Daniel Hägele.
 #
-#    Copyright (c) 2020 and later, Markus Sifft and Daniel Hägele.
-#    All rights reserved.
-#
-#    Redistribution and use in source and binary forms, with or without
-#    modification, are permitted provided that the following conditions are
-#    met:
-#
-#    1. Redistributions of source code must retain the above copyright notice,
-#       this list of conditions and the following disclaimer.
-#
-#    2. Redistributions in binary form must reproduce the above copyright
-#       notice, this list of conditions and the following disclaimer in the
-#       documentation and/or other materials provided with the distribution.
-#
-#    3. Neither the name signalsnap: Signal Analysis In Python Made Easy nor the
-#       names of its contributors may be used to endorse or promote products
-#       derived from this software without specific prior written permission.
-#
-#    THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
-#    "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
-#    LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
-#    PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
-#    HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
-#    SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
-#    LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
-#    DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
-#    THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
-#    (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
-#    OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-###############################################################################
+# This software is provided under the terms of the 3-Clause BSD License.
+# For details, see the LICENSE file in the root of this repository or
+# https://opensource.org/licenses/BSD-3-Clause
 
 import h5py
 import matplotlib.colors as colors
