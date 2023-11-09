226d09e257123b1a910d2ae7f4991cbde1b82ded
Markus Sifft <m.sifft@icloud.com>
2023-11-09 15:20:45 +0100

    testing a faster implementation of c4 estimator

--- src/signalsnap/spectrum_calculator.py
+++ src/signalsnap/spectrum_calculator.py
@@ -675,25 +675,25 @@ class SpectrumCalculator:
         y = conj(x)
         w = conj(z)
 
-        x_mean = x - mean(x, dim=2)
-        y_mean = y - mean(y, dim=2)
-        z_mean = z - mean(z, dim=2)
-        w_mean = w - mean(w, dim=2)
-
-        xyzw = af.matmulNT(x_mean * y_mean, z_mean * w_mean)
-        xyzw_mean = mean(xyzw, dim=2)
-
-        xy_mean = mean(x_mean * y_mean, dim=2)
-        zw_mean = mean(z_mean * w_mean, dim=2)
-        xy_zw_mean = af.matmulNT(xy_mean, zw_mean)
-
-        xz_mean = af.matmulNT(x_mean, z_mean)
-        yw_mean = af.matmulNT(y_mean, w_mean)
-        xz_yw_mean = mean(xz_mean * yw_mean, dim=2)
-
-        xw_mean = af.matmulNT(x_mean, w_mean)
-        yz_mean = af.matmulNT(y_mean, z_mean)
-        xw_yz_mean = mean(xw_mean * yz_mean, dim=2)
+        # x_mean = x - mean(x, dim=2)
+        # y_mean = y - mean(y, dim=2)
+        # z_mean = z - mean(z, dim=2)
+        # w_mean = w - mean(w, dim=2)
+        #
+        # xyzw = af.matmulNT(x_mean * y_mean, z_mean * w_mean)
+        # xyzw_mean = mean(xyzw, dim=2)
+        #
+        # xy_mean = mean(x_mean * y_mean, dim=2)
+        # zw_mean = mean(z_mean * w_mean, dim=2)
+        # xy_zw_mean = af.matmulNT(xy_mean, zw_mean)
+        #
+        # xz_mean = af.matmulNT(x_mean, z_mean)
+        # yw_mean = af.matmulNT(y_mean, w_mean)
+        # xz_yw_mean = mean(xz_mean * yw_mean, dim=2)
+        #
+        # xw_mean = af.matmulNT(x_mean, w_mean)
+        # yz_mean = af.matmulNT(y_mean, z_mean)
+        # xw_yz_mean = mean(xw_mean * yz_mean, dim=2)
 
         #s4 = m ** 2 / ((m - 1) * (m - 2) * (m - 3)) * (
         #        (m + 1) * xyzw_mean -
