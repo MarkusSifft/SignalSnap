Corrected calculation of S_err to be the error of the mean

src/signalsnap/spectrum_calculator.py
@@ -1272,7 +1272,7 @@ class SpectrumCalculator:
             if not self.config.turbo_mode:
                 #self.S_err[order] /= n_windows // self.config.m_var * np.sqrt(n_windows)
 
-                self.S_err[order] = (n_windows // self.config.m_var) * np.sqrt(self.S_err[order])
+                self.S_err[order] = 1/(n_windows // self.config.m_var) * np.sqrt(self.S_err[order])
 
                 if self.config.interlaced_calculation:
                     self.S_err[order] /= 2